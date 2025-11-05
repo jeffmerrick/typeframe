7b1bb8da9f2fbd8c648fb057bcf045992335ba24
Jeff Merrick <jeff@wireform.io>
2025-11-05 11:01:38 -0600

    Deploy website - based on d2a5483bfa13bf42de9c0c3882a7f0ee907de7eb

--- px-88/software/brightness_down.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Decrease brightness by 20
-BF=$(ls /sys/class/backlight/*/brightness)
-CUR=$(cat $BF)
-NEW=$((CUR-20))
-[ $NEW -lt 0 ] && NEW=0
-echo $NEW | sudo tee $BF
--- px-88/software/keyboard/README.md
+++
@@ -1,1 +0,0 @@
-Keymaps for MK Point65 PCB from https://mechanicalkeyboards.com/products/mk-point65-keyboard-kit
